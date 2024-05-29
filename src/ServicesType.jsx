8b67ccb000716ea4b26593530bd4d0e930991e27
aditya <asadityasingh2572000@gmail.com>
2024-05-29 18:03:30 +0530

    change images and content

--- src/ServicesType.jsx
+++ src/ServicesType.jsx
@@ -3,37 +3,37 @@ import "./ServicesType.css";
 
 let typeOfservices = [
   {
-    images: "./public/images/img_06.jpg",
+    images: "./public/images/graphic.png",
     servic_name: "Graphic Design",
     dsscription:
       " Our graphic design services cover a broad spectrum, from creating eye-catching logos to designing comprehensive brand identities. We focus on ensuring that every element we create aligns perfectly with your brand's message and aesthetic ",
   },
   {
-    images: "./public/images/img_09.jpg",
+    images: "./public/images/webDesingn2.png",
     servic_name: "Web Design",
     dsscription:
       " In the digital age, a well-designed website is crucial. Our web design services offer intuitive, user-friendly, and aesthetically pleasing websites that provide an exceptional user experience. website optimized for performance and responsiveness across all devices. ",
   },
   {
-    images: "./public/images/img_07.jpg",
+    images: "./public/images/uxui1.jpg",
     servic_name: "UI/UX Design",
     dsscription:
       " User Interface (UI) and User Experience (UX) design are critical for any digital product. Our UI/UX designers craft interfaces that are not only beautiful but also highly functional, ensuring that your users enjoy a seamless and engaging experience.",
   },
   {
-    images: "./public/images/img_10.jpg",
+    images: "./public/images/print-3.jpg",
     servic_name: " Print Design",
     dsscription:
       "Despite the digital shift, print media remains a powerful tool for communication. Our print design services include brochures, business cards, posters, and more, all designed to leave a lasting impression.",
   },
   {
-    images: "./public/images/img_08.jpg",
+    images: "./public/images/Social-Media-Aesth.png",
     servic_name: " Social Media Design",
     dsscription:
       " In today's social media-driven world, having visually compelling content is more important than ever. We create engaging and on-brand graphics for all your social media platforms, ensuring your content stands out and captures your audience's attention.",
   },
   {
-    images: "./public/images/img_11.jpg",
+    images: "./public/images/packaging11.jpg",
     servic_name: " Packaging Design",
     dsscription:
       "Our packaging design encapsulates the essence of [Company Name], reflecting its core values of innovation, quality, and sustainability. The design is modern yet timeless, ensuring it appeals to our target demographic while standing out on the shelf.",
@@ -45,9 +45,9 @@ const ServicesType = () => {
     <div className="">
       <div className="text-center ">
         <div className="containers">
-          <p className="heading">What We Offers</p>
+          <p className="heading">Our Serviceses</p>
           <p className="services-title">
-            We Provide Lot’s Of Printing &<br></br> Branding Service
+            We Provide Lot’s Of Design &<br></br> Branding Service
           </p>
 
           <div className="d-flex flex-wrap justify-content-center col-12">
--- src/components/Nav1/Na1.jsx
+++ src/components/Nav1/Na1.jsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Offcanvas from 'react-bootstrap/Offcanvas';
-import image from '../../images/logo.png'
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import image from "../../images/logo.png";
 import "./Nav1.css";
 
 function Na1() {
   return (
     <>
-      {[ 'md' ].map((expand) => (
-        <Navbar key={expand} expand={expand} className="nav1 bg-nav mb-3 position-fixed top-0 w-100 z-1">
+      {["md"].map((expand) => (
+        <Navbar
+          key={expand}
+          expand={expand}
+          className="nav1 bg-nav mb-3 position-fixed top-0 w-100 z-3"
+        >
           <Container className="Container fluid ">
-
-          <a className="navbar-brand" href="#">
-          <img
-            src={image} 
-            alt="Logo"
-            style={{ width: 'clamp(4rem, 8vw, 5rem)', marginRight: '10px' }}
-          />
-         
-        </a>
+            <a className="navbar-brand" href="#">
+              <img
+                src={image}
+                alt="Logo"
+                style={{ width: "clamp(4rem, 8vw, 5rem)", marginRight: "10px" }}
+              />
+            </a>
             <Navbar.Brand href="#">Shreem Enterprices</Navbar.Brand>
             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
             <Navbar.Offcanvas
@@ -29,19 +31,29 @@ function Na1() {
               placement="end"
             >
               <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                
-                </Offcanvas.Title>
+                <Offcanvas.Title
+                  id={`offcanvasNavbarLabel-expand-${expand}`}
+                ></Offcanvas.Title>
               </Offcanvas.Header>
-              <Offcanvas.Body >
+              <Offcanvas.Body>
                 <Nav className="list justify-content-end flex-grow-1 pe-3  gap-5 ">
-                  <Nav.Link className='nav-item blackl' href="#home">Home</Nav.Link>
-                  <Nav.Link className='nav-item blackl' href="#about" >About</Nav.Link>
-                  <Nav.Link className='nav-item blackl' href="#services">Services</Nav.Link>
-                  <Nav.Link className='nav-item blackl' href="#orderServices">Order Request</Nav.Link>
-                  <Nav.Link className='nav-item blackl' href="#contact">Contact</Nav.Link>
+                  <Nav.Link className="nav-item blackl" href="#home">
+                    Home
+                  </Nav.Link>
+                  <Nav.Link className="nav-item blackl" href="#about">
+                    About
+                  </Nav.Link>
+                  <Nav.Link className="nav-item blackl" href="#services">
+                    Services
+                  </Nav.Link>
+                  <Nav.Link className="nav-item blackl" href="#orderServices">
+                    Order Request
+                  </Nav.Link>
+                  <Nav.Link className="nav-item blackl" href="#contact">
+                    Contact
+                  </Nav.Link>
                 </Nav>
-               </Offcanvas.Body>
+              </Offcanvas.Body>
             </Navbar.Offcanvas>
           </Container>
         </Navbar>
